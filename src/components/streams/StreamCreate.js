557eafe86e3b1d769a2288c6d136d6692d589cfe
Kurt Hammes <kilowatt11@cableone.net>
2019-01-30 22:48:44 -0700

    Created Validate function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -12,15 +12,34 @@ class StreamCreate extends React.Component {
         )
 
     }
+
+    onSubmit(formValues){
+        console.log(formValues)
+    }
     render() {
         return (
-            <form className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
 };
+
+const validate = (formValues) =>{
+    const errors = {
+
+    }
+    if(!formValues.title){
+        errors.title ='You must enter a title';
+    }
+    if(!formValues.description){
+        errors.description = 'You must enter a description';
+    }
+
+    return errors
+}
 
 export default reduxForm({
     form: 'streamCreate'
