36301f8d1700c770c75bdb3751e5bbba56057ab6
Kurt Hammes <kilowatt11@cableone.net>
2019-02-04 21:06:56 -0700

    Added error message for validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,24 +2,35 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label, meta }) {
+    renderError = ({error, touched}) => {
+        if(touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    };
+
+    renderInput =({ input, label, meta }) => {
         
         return (
             <div className="field">
                 <label >{label}</label>
                 <input {...input} autoComplete="off"/>
-                <div>{meta.error}</div>
+                {this.renderError(meta)}
             </div>
         )
 
     }
+
 
     onSubmit(formValues) {
         console.log(formValues)
     }
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
