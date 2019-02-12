ba129e1ba93a59ec03f513fcc166c1aa7b7e363a
Kurt Hammes <kilowatt11@cableone.net>
2019-02-11 22:10:41 -0700

    handle response and dispatch action

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,6 +12,8 @@ export const signOut = () => {
     }
 };
 
-export const createStream= formValues => async dispatch => {
-    streams.post('/streams', formValues)
+export const createStream = formValues => async dispatch => {
+   const response = await streams.post('/streams', formValues);
+
+   dispatch({type:'CREATE_STREAM',payload:response.data})
 };
