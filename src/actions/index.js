a86360c352e0a6e3d25b9c17aae910505b19a3de
Kurt Hammes <kilowatt11@cableone.net>
2019-02-11 22:19:49 -0700

    Action Creaters

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,3 +17,23 @@ export const createStream = formValues => async dispatch => {
 
    dispatch({type:'CREATE_STREAM',payload:response.data})
 };
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+ 
+    dispatch({type:'FETCH_STREAM',payload:response.data})
+ };
+
+ export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({type:'FETCH_STREAM', payload:response.data})
+ };
+
+ export const editStream = (id,formValues) => async dispatch =>{
+    const response = await streams.put(`/streams/${id}`,formValues)
+    dispatch({type:'EDIT_STREAM',payload:response.data});
+ };
+
+ export const deleteStream = (id) => async dispatch =>{
+     await streams.delete(`/streams/${id}`);
+ }
